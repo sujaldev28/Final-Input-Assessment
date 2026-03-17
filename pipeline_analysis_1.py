a7e6577e7aaeddd622d30c9d32cf4c262c914ad8
sujaldev28 <sujalhadge12@gmail.com>
2026-03-17 15:49:15 +0530

    Challenge-1 completed

--- pipeline_analysis_1.py
+++ pipeline_analysis_1.py
@@ -1,12 +1,3 @@
-# Challenge 1: The Data Profiler (Easy)
-# Goal: Master basic data manipulation and string formatting.
-# ● Topics: Variables, Data Types, Strings, Lists.
-# ● Task: Create a script that takes a list of "raw" sensor readings (strings with extra
-# whitespace and mixed casing) and converts them into a clean, numerical list.
-# ● Objective: 1. Create a list of strings: [" 23.5 ", "low", " 45.0", "HIGH ", "30.2"].
-# 2. Use a loop to strip whitespace and convert all strings to lowercase.
-# 3. Identify which values are numbers and store them in a new list as floats.
-
 raw_readings = [" 23.5 ", "low", " 45.0", "HIGH ", "30.2"]
 clean_readings = []
 clean_numeric_readings = []
